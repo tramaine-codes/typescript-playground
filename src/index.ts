aca8b07567903be1d4f356e7630d5d591f1a35b8
Tramaine Gillus <1573935+tgillus@users.noreply.github.com>
2024-06-02 22:05:34 -0400

    chore: clean up

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 type Foo = [firstName: string, lastName: string];
 
+// @ts-expect-error
+['Jane'] satisfies Foo;
 ['Jane', 'Doe'] satisfies Foo;
 
 type Bar = [firstName: string, lastName: string, '!'];
 
+// @ts-expect-error
+['Jane', 'Doe'] satisfies Bar;
 ['Jane', 'Doe', '!'] satisfies Bar;
