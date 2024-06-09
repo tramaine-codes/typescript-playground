4e5b9127dcb24cff5f7c1dd7381f7239bbe35010
Tramaine Gillus <1573935+tgillus@users.noreply.github.com>
2024-06-09 16:24:13 -0400

    feat: add types and values exercises

---
+++ src/01/types-and-values.ts
@@ -0,0 +1,24 @@
+import assert from 'node:assert';
+
+type SomeFunction<A, B> = [A, B];
+type Foo = SomeFunction<number, string>;
+
+[1, 'bar'] satisfies Foo;
+
+type DoNothing<T> = T;
+type Baz = DoNothing<boolean>;
+
+true satisfies Baz;
+
+function identity<T>(a: T): T {
+  return a;
+}
+
+assert.deepStrictEqual(identity(1), 1);
+
+function safeHead<T>(arr: T[], defaultValue: T): T {
+  return arr[0] ?? defaultValue;
+}
+
+assert.deepStrictEqual(safeHead<number>([], 0), 0);
+assert.deepStrictEqual(safeHead(['qux', 'quux'], 'none'), 'qux');
