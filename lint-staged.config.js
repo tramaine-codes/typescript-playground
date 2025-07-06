1907c19f349c6a0a4d06dd8cd9d909f5d277ffdc
Tramaine G. <github@tramaine.dev>
2025-07-05 22:06:56 -0400

    chore: upgrade dependencies

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 export default {
   '*.{cjs,js,json,jsx,mjs,ts,tsx,md}':
-    'biome format --write --no-errors-on-unmatched',
-  '*.{cjs,js,jsx,mjs,ts,tsx}': 'biome lint --apply --no-errors-on-unmatched',
+    'biome format --fix --no-errors-on-unmatched',
+  '*.{cjs,js,jsx,mjs,ts,tsx}': 'biome lint --fix --no-errors-on-unmatched',
 };
